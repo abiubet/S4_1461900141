f943450fac24e975bcdfafe6a34b408048161f33
abi <abiubetassabi@gmail.com>
2021-06-12 20:00:01 +0700

    terbaru

--- resources/views/home-0141.blade.php
+++ resources/views/home-0141.blade.php
@@ -9,6 +9,20 @@
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 </head>
 
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <h4 class="text-white">PTW</h4>
+      <span class="text-muted">Pengembangan Teknologi Web</span>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</div>
+
 <body>
     <div class="container mt-3">
         <h2 class="mt-5">Data Dokter</h2>
